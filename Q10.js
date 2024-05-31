96793fd05ba323830ecfacb4345e58163fcafa55
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:29:48 +0500

    Add files via upload

---
+++ Q10.js
@@ -0,0 +1,5 @@
+//Qus no 10
+// insert the elements "pineapple" and "pear" at index 2 of the fruits array using the method
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.splice(2, 0, "pineapple", "pear");
+console.log(fruits);
---
+++ Q10.ts
@@ -0,0 +1,5 @@
+//Qus no 10
+// insert the elements "pineapple" and "pear" at index 2 of the fruits array using the method
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.splice(2,0,"pineapple","pear")
+console.log(fruits);
