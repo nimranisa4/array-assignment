859303d86f19e8c823adac7dd0c502c7ee624b2e
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:31:32 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,4 @@
+//create a new array named lastTwoFrutis that contains the last two elements of the fruits array using the method
+var lastTwoFrutis = ["apple", "banna", "mango", "orange"];
+lastTwoFrutis.splice(-2);
+console.log(lastTwoFrutis);
---
+++ Q12.ts
@@ -0,0 +1,4 @@
+//create a new array named lastTwoFrutis that contains the last two elements of the fruits array using the method
+let lastTwoFrutis : string [] = ["apple", "banna", "mango", "orange"];
+lastTwoFrutis.splice(-2)
+console.log(lastTwoFrutis);
