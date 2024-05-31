1ac51064d1b02e0f01f249cc4c9e57ea11e8f5ea
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:28:23 +0500

    Add files via upload

---
+++ Q8.js
@@ -0,0 +1,5 @@
+//Qus no 8 
+//Add the element "kiwi" to the beginning of the fruits array using the method.
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.unshift("kiwi");
+console.log(fruits);
---
+++ Q8.ts
@@ -0,0 +1,5 @@
+//Qus no 8 
+//Add the element "kiwi" to the beginning of the fruits array using the method.
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.unshift("kiwi")
+console.log(fruits);
