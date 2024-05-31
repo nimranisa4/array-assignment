11fc97ffc886ff4d3be4a86d9d5a0538b22085ae
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:25:28 +0500

    Add files via upload

---
+++ Q4.js
@@ -0,0 +1,5 @@
+//Qus no 4 
+//chang the second element of the numbers array to 25.
+var number = [10, 20, 30, 40];
+number[1] = 25;
+console.log(number);
---
+++ Q4.ts
@@ -0,0 +1,8 @@
+//Qus no 4 
+//chang the second element of the numbers array to 25.
+let number : number[] = [10,20,30,40]
+    number[1] =25
+
+console.log(number);
+
+
