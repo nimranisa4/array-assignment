3dcd9cec13fa4ddb8c9ea6c195b3e38e2ec1cbdb
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:28:53 +0500

    Add files via upload

---
+++ Q9.js
@@ -0,0 +1,5 @@
+//question 9
+//remove 2 elements from the fruit array string from index 1 using the method
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.splice(-2); // (-2)
+console.log(fruits);
---
+++ Q9.ts
@@ -0,0 +1,5 @@
+//question 9
+//remove 2 elements from the fruit array string from index 1 using the method
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.splice(1,2) // (-2)
+console.log(fruits);
