a92971d18647952827e45c7ccbe6ee0268109569
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:26:38 +0500

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,5 @@
+//Qus no 5 
+// add the element "grape" to the end of the fruits array using the method.
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.push("graps");
+console.log(fruits);
---
+++ Q5.ts
@@ -0,0 +1,5 @@
+//Qus no 5 
+// add the element "grape" to the end of the fruits array using the method.
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.push("graps")
+console.log(fruits);
