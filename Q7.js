1940d80a5867f29bbbe7324a65e6b6e2ee47f929
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:27:53 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,5 @@
+//Qus no 7
+//remove the first element from the fruits array using the method and assign it to a variable named firstFruit.
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.shift();
+console.log(fruits);
---
+++ Q7.ts
@@ -0,0 +1,5 @@
+//Qus no 7
+//remove the first element from the fruits array using the method and assign it to a variable named firstFruit.
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.shift()
+console.log(fruits);
