db639021aa4b0b842648e2339899c08ce22f7649
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:30:37 +0500

    Add files via upload

---
+++ Q11.ts
@@ -0,0 +1,5 @@
+//qus no 11
+//create a new array named citrusfruits that contains the first two elements of the fruits array using the method
+let citrusFruits : string [] = ["apple", "banana", "mango", "orange"];
+ citrusFruits = citrusFruits.splice(0,2)
+console.log(citrusFruits);
