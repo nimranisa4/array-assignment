1953c5f6afe6f9a825996149bce57a2da287e7b0
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:27:16 +0500

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,5 @@
+// Qus no 6 
+//remove the last element from the fruits array using the method and assign it to a variable named lastFruits
+var fruits = ["apple", "banana", "mango", " Orange"];
+fruits.pop();
+console.log(fruits);
---
+++ Q6.ts
@@ -0,0 +1,5 @@
+// Qus no 6 
+//remove the last element from the fruits array using the method and assign it to a variable named lastFruits
+const fruits : string[] = [ "apple", "banana", "mango", " Orange"]
+fruits.pop()
+console.log(fruits);
