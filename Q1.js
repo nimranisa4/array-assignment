91f74e7644765364c1b8689ad4bac803ea85cb95
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:23:35 +0500

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,5 @@
+// Qus no 1
+//create an array named fruits that contains the following 
+//string elements: "apple", "banana", "mango", " Orange"
+var fruits = ["apple", "banana", "mango", " Orange"];
+console.log(fruits);
