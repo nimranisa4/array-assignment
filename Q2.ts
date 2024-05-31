2700a276d7091cce4efcd027a25dac40151f5063
nimranisa4 <168928017+nimranisa4@users.noreply.github.com>
2024-05-31 20:24:24 +0500

    Add files via upload

---
+++ Q2.ts
@@ -0,0 +1,5 @@
+// Qus no 2
+// Declare an array named numbers that can contain only number elements and 
+// initialize it with the values 10,20,30,and 40.
+let number : number[] = [10,20,30,40]
+console.log(number);
